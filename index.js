480f8a6ac506e8bf907220c8361e03f09d4e5bc1
Clay Walker <clayzermk1@gmail.com>
2014-06-02 15:39:02 -0700

    Initial release.

--- index.js
+++ index.js
@@ -2,57 +2,30 @@ var PubSub = require('mongodb-pubsub')
   , EventEmitter = require('events').EventEmitter;
 
 exports.attach = function (options) {
-  var amino = this
-    , subscriber = new EventEmitter()
-    , client;
+  var amino = this;
 
   if (typeof options === 'string') {
-    options = { "uri": options, "collection": options.collection || "buffer" };
+    options = { "uri": (options || {}), "collection": (options || {}).collection || "buffer" };
   }
   else {
-    options = { "uri": options.uri || "mongodb://localhost:27017/amino", "collection": options.collection || "buffer" };
+    options = { "uri": (options || {}).uri || "mongodb://localhost:27017/amino", "collection": (options || {}).collection || "buffer" };
   }
 
-  amino.mongodb = client = new PubSub(options.uri, options.collection);
+  amino.mongodb = new PubSub(options.uri, options.collection);
 
-  subscriber.setMaxListeners(0);
-
-  client.on('error', amino.emit.bind(amino, 'error'));
-  client.on('subscribe', amino.emit.bind(amino, 'subscribe'));
-  client.on('unsubscribe', amino.emit.bind(amino, 'unsubscribe'));
-
-  client.on('message', function (ev, packet) {
-    try {
-      subscriber.emit.apply(subscriber, packet.args);
-    }
-    catch (e) {
-      amino.emit('error', e);
-    }
+  amino.mongodb.open(function () {
+    amino.mongodb._channels.setMaxListeners(0);
+    amino.mongodb.connected = (amino.mongodb._db._state === 'connected'); // monkey-patch the db state
   });
 
-  amino.publish = function () {
-    try {
-      client.publish.apply(client, arguments);
-    }
-    catch (e) {
-      amino.emit('error', e);
-    }
-  };
+  // alias events
+  amino.mongodb.on('error', amino.emit.bind(amino, 'error'));
+  amino.mongodb.on('publish', amino.emit.bind(amino, 'publish'));
+  amino.mongodb.on('subscribe', amino.emit.bind(amino, 'subscribe'));
+  amino.mongodb.on('unsubscribe', amino.emit.bind(amino, 'unsubscribe'));
 
-  amino.subscribe = function (ev, handler) {
-    subscriber.on(ev, handler);
-    client.subscribe(ev, function () {});
-  };
-
-  amino.unsubscribe = function (ev, handler) {
-    if (handler) {
-      subscriber.removeListener(ev, handler);
-    }
-    else {
-      subscriber.removeAllListeners(ev);
-    }
-    if (subscriber.listeners(ev).length === 0) {
-      client.unsubscribe(ev);
-    }
-  };
+  // alias functions
+  amino.publish = amino.mongodb.publish.bind(amino.mongodb);
+  amino.subscribe = amino.mongodb.subscribe.bind(amino.mongodb);
+  amino.unsubscribe = amino.mongodb.unsubscribe.bind(amino.mongodb);
 };
